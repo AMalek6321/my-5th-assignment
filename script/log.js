f63f2f07a7384154f766f0abd8e71a78f79ea29a
Malek <smmalek3@gmail.com>
2025-03-02 11:31:01 +0600

    js first commit

---
+++ script/log.js
@@ -0,0 +1,26 @@
+const btn = document.getElementById('changeColorBtn');
+const body = document.body;
+
+function getRandomColor() {
+  const letters = '0123456789ABCDEF';
+  let color = '#';
+  for (let i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
+
+btn.addEventListener('click', () => {
+  body.style.backgroundColor = getRandomColor();
+});
+// // next
+document.getElementById('shop-ease-btn').addEventListener('click', function() {
+  alert('Board Updated Successfully!');
+  this.disabled = true;
+  var newElement = document.createElement('p');
+  newElement.textContent = 'You have completed the task Fix Mobile Button Issue';
+  document.getElementById('text-add').appendChild(newElement);
+});
+
+
+
