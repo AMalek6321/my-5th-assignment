69e8699d7c59bf38b15a09c40b46f2f0ba0e5851
Malek <smmalek3@gmail.com>
2025-03-02 12:35:52 +0600

    click card button done

--- script/log.js
+++ script/log.js
@@ -13,7 +13,7 @@ function getRandomColor() {
 btn.addEventListener('click', () => {
   body.style.backgroundColor = getRandomColor();
 });
-// // next
+// // card 1
 document.getElementById('shop-ease-btn').addEventListener('click', function() {
   alert('Board Updated Successfully!');
   this.disabled = true;
@@ -26,6 +26,7 @@ document.getElementById('shop-ease-btn').addEventListener('click', function() {
   newElement.style.borderRadius = '10px';
   newElement.style.textAlign = 'center';
   newElement.style.padding = '4px';
+  newElement.style.margin = '12px';
   var number = parseInt(document.getElementById('task-count').textContent);
   number++;
   document.getElementById('task-count').textContent = number;
@@ -33,6 +34,116 @@ document.getElementById('shop-ease-btn').addEventListener('click', function() {
   number--;
   document.getElementById('task-count-2').textContent = number;
 });
+// card 2
+document.getElementById('soft-pay-btn').addEventListener('click', function() {
+  alert('Board Updated Successfully!');
+  this.disabled = true;
+  var newElement = document.createElement('p');
+  newElement.textContent = 'You have completed the task Add Pay Success Modal';
+  var currentTime = new Date();
+  newElement.textContent += ' at ' + currentTime.toLocaleString();
+  document.getElementById('text-add').appendChild(newElement);
+  newElement.style.backgroundColor = 'white';
+  newElement.style.borderRadius = '10px';
+  newElement.style.textAlign = 'center';
+  newElement.style.padding = '4px';
+  newElement.style.margin = '12px';
+  var number = parseInt(document.getElementById('task-count').textContent);
+  number++;
+  document.getElementById('task-count').textContent = number;
+  var number = parseInt(document.getElementById('task-count-2').textContent);
+  number--;
+  document.getElementById('task-count-2').textContent = number;
+});
+
+// card 3
+document.getElementById('meta-btn').addEventListener('click', function() {
+  alert('Board Updated Successfully!');
+  this.disabled = true;
+  var newElement = document.createElement('p');
+  newElement.textContent = 'You have completed the task Add new reaction';
+  var currentTime = new Date();
+  newElement.textContent += ' at ' + currentTime.toLocaleString();
+  document.getElementById('text-add').appendChild(newElement);
+  newElement.style.backgroundColor = 'white';
+  newElement.style.borderRadius = '10px';
+  newElement.style.textAlign = 'center';
+  newElement.style.padding = '4px';
+  newElement.style.margin = '12px';
+  var number = parseInt(document.getElementById('task-count').textContent);
+  number++;
+  document.getElementById('task-count').textContent = number;
+  var number = parseInt(document.getElementById('task-count-2').textContent);
+  number--;
+  document.getElementById('task-count-2').textContent = number;
+});
+
+// card 4
+document.getElementById('prog-hero-btn').addEventListener('click', function() {
+  alert('Board Updated Successfully!');
+  this.disabled = true;
+  var newElement = document.createElement('p');
+  newElement.textContent = 'You have completed the task Fix Video Loading Issue';
+  var currentTime = new Date();
+  newElement.textContent += ' at ' + currentTime.toLocaleString();
+  document.getElementById('text-add').appendChild(newElement);
+  newElement.style.backgroundColor = 'white';
+  newElement.style.borderRadius = '10px';
+  newElement.style.textAlign = 'center';
+  newElement.style.padding = '4px';
+  newElement.style.margin = '12px';
+  var number = parseInt(document.getElementById('task-count').textContent);
+  number++;
+  document.getElementById('task-count').textContent = number;
+  var number = parseInt(document.getElementById('task-count-2').textContent);
+  number--;
+  document.getElementById('task-count-2').textContent = number;
+});
+
+// card 5
+document.getElementById('google-btn').addEventListener('click', function() {
+  alert('Board Updated Successfully!');
+  this.disabled = true;
+  var newElement = document.createElement('p');
+  newElement.textContent = 'You have completed the task Integrate AI search';
+  var currentTime = new Date();
+  newElement.textContent += ' at ' + currentTime.toLocaleString();
+  document.getElementById('text-add').appendChild(newElement);
+  newElement.style.backgroundColor = 'white';
+  newElement.style.borderRadius = '10px';
+  newElement.style.textAlign = 'center';
+  newElement.style.padding = '4px';
+  newElement.style.margin = '12px';
+  var number = parseInt(document.getElementById('task-count').textContent);
+  number++;
+  document.getElementById('task-count').textContent = number;
+  var number = parseInt(document.getElementById('task-count-2').textContent);
+  number--;
+  document.getElementById('task-count-2').textContent = number;
+});
+
+// card 6
+document.getElementById('polygon-btn').addEventListener('click', function() {
+  alert('Board Updated Successfully!');
+  this.disabled = true;
+  var newElement = document.createElement('p');
+  newElement.textContent = 'You have completed the task Fix Review Ami Probashi Site';
+  var currentTime = new Date();
+  newElement.textContent += ' at ' + currentTime.toLocaleString();
+  document.getElementById('text-add').appendChild(newElement);
+  newElement.style.backgroundColor = 'white';
+  newElement.style.borderRadius = '10px';
+  newElement.style.textAlign = 'center';
+  newElement.style.padding = '4px';
+  newElement.style.margin = '12px';
+  var number = parseInt(document.getElementById('task-count').textContent);
+  number++;
+  document.getElementById('task-count').textContent = number;
+  var number = parseInt(document.getElementById('task-count-2').textContent);
+  number--;
+  document.getElementById('task-count-2').textContent = number;
+});
+
 
  
 
